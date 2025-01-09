bfa423fde601502b56ed1b66edc6a6a91b42b232
‘lm’ <liming163vip@163.com>
2025-01-09 14:50:40 +0800

    [fix] Publish use invalid access_token

--- src/service/api.js
+++ src/service/api.js
@@ -4,8 +4,7 @@ axios.defaults.baseURL = "/api/v1/";
 axios.interceptors.request.use((config) => {
   let loginResult = JSON.parse(localStorage.getItem("loginResult"));
   if (loginResult) { 
-	// const token = loginResult.access_token
-	const token = loginResult.token
+	const token = loginResult.access_token
 	config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
