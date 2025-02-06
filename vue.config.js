2a245374ec750d17ece18b2b73bf7e73f3c60f15
lcf163 <liming163vip@163.com>
2025-02-06 22:55:30 +0800

    [update] add env config, dev and prod

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,20 @@
 module.exports = {
     assetsDir: "static",
+    productionSourceMap: false,
+    
+    // 开发环境配置
     devServer: {
         proxy: {
             '/api/v1': {
-              target: 'http://127.0.0.1:8081',
-              changeOrigin: true,
+                target: process.env.VUE_APP_API_BASE_URL,
+                changeOrigin: true,
             }
-        }
-    }
-  }
+        },
+        // historyApiFallback: true  // 开发环境支持 history 模式
+    },
+
+    // 生产环境配置
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/'  // 生产环境路径
+        : '/', // 开发环境路径
+}
